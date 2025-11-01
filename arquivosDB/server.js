196a5cba5ce6942e03b261b8b46fd642d704a160
giovanalimads <gilimaolliveira@gmail.com>
2025-11-01 12:15:01 -0300

    commit

--- arquivosDB/server.js
+++ arquivosDB/server.js
@@ -12,7 +12,7 @@ const dbConfig = {
     server: "localhost",
     database: "SistemaPIM",
     options: {
-        trustedConnection: true,
+        encrypt: false,
         trustServerCertificate: true
     }
 };
